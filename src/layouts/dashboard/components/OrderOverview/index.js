16ada83b931528ff834021c8ed76d29438aecd92
Ahmad Moltafet <moltafet.ahmad2015@gmail.com>
2023-01-27 17:52:52 -0800

    g

--- src/layouts/dashboard/components/OrderOverview/index.js
+++ src/layouts/dashboard/components/OrderOverview/index.js
@@ -8,8 +8,52 @@ import SuiButton from "components/SuiButton";
 // Soft UI Dashboard React examples
 import TimelineItem from "examples/Timeline/TimelineItem";
 import { height } from "@mui/system";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function OrdersOverview() {
+  const [modules, setModules] = useState([]);
+  let navigate = useNavigate();
+
+  const handleNavigateModule = (embedUrl) => {
+    const modelId = embedUrl.split('/')[4];
+    sessionStorage.setItem('ModelId', modelId);
+    navigate('/modules/module')
+  }
+
+  useEffect(() => {
+    const API_TOKEN = sessionStorage.getItem('SketchFab');
+    async function getCollection() {
+        const collectionName = 'a06673c9f16c460d87ea064a952227d8';
+        const url = `https://api.sketchfab.com/v3/collections/${collectionName}/models`;
+        const myCollectionsEndpoint =  url;
+        const headers = {
+            Authorization: `Token ${API_TOKEN}`,
+        };
+        try {
+            const response = await axios.get(myCollectionsEndpoint, { headers });
+            return response.data;
+        } catch (error) {
+            console.log(`An API error occurred: ${error}`);
+            process.exit(1);
+        }
+    }
+    async function main() {
+        console.log('Getting models from your profile...');
+        const collection = await getCollection();
+
+        if (!collection) {
+            console.log("You don't seem to have any collection, let's create one!");
+            return;
+        } else {
+            console.log('Your collections:' + collection.results);
+            setModules(collection.results);
+        }
+    }
+    main();
+  }, [])
+
   return (
     <Card className="h-100" sx={{
       display: "flex",
@@ -42,97 +86,36 @@ function OrdersOverview() {
     
       <SuiBox p={2}>
       <Grid container spacing={2}>
-          <Grid item xs={12} sm={6} md={4} xl={3}>
-            <Card style={{ width: "100%", height: "17rem"  }}>
-              <div class="sketchfab-embed-wrapper"> 
-                <iframe sandbox="allow-same-origin allow-scripts" width={"100%"} title="" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/2db0c6813c9b471e9e9eb2b4d8a01d08/embed"> 
-                </iframe> 
-                <TimelineItem
-                  title="Wolf"
-                  description="The wolf is a canine native to the wilderness and remote areas of Eurasia and North America."
-                  dateTime="12 March 2023"
-                />
-              </div>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} xl={3}>
-              <Card className="h-100" style={{ width: "100%", height: "17rem"  }}>
-              <div class="sketchfab-embed-wrapper"> 
-                <iframe sandbox="allow-same-origin allow-scripts" width={"100%"} title="" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/e402d3d541eb4b199c57d5410f5d3c57/embed"> 
-                </iframe> 
-                <TimelineItem
-                  title="Human Muscle Group"
-                  description="The human body is composed of many types of muscle tissue."
-                  dateTime="12 March 2023"
-                />
-              </div>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} xl={3}>
-            <Card className="h-100" style={{ width: "100%", height: "17rem"  }}>
-              <div class="sketchfab-embed-wrapper"> 
-                <iframe sandbox="allow-same-origin allow-scripts" width={"100%"} title="" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/ce09f4099a68467880f46e61eb9a3531/embed"> 
-                </iframe> 
-                <TimelineItem
-                  title="Human Lungs"
-                  description="The human lungs are a pair of spongy organs in the chest of humans and some other animals."
-                  dateTime="12 March 2023"
-                />
-              </div>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} xl={3}>
-            <Card className="h-100" style={{ width: "100%", height: "17rem"  }}>
-              <div class="sketchfab-embed-wrapper"> 
-                <iframe sandbox="allow-same-origin allow-scripts" width={"100%"} frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/b3328386cff14905a60ea1a44db17d65/embed"> 
-                </iframe> 
-                <TimelineItem
-                  title="Flavin Adenine Dinucleotide"
-                  description="FAD is a coenzyme composed of an adenosine monophosphate and a flavin mononucleotide bound by a pyrophosphate bond."
-                  dateTime="12 March 2023"
-                />
-              </div>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} xl={3}>
-            <Card className="h-100" style={{ width: "100%", height: "17rem"  }}>
-              <div class="sketchfab-embed-wrapper"> 
-                <iframe sandbox="allow-same-origin allow-scripts" width={"100%"} frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="false" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/6991ad7fb51341618f5c257b5ce9b204/embed"> 
-                </iframe> 
-                <TimelineItem
-                  title="Human Brain"
-                  description="The human brain is the central organ of the human nervous system, and with the spinal cord makes up the central nervous system."
-                  dateTime="12 March 2023"
-                />
-              </div>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} xl={3}>
-            <Card className="h-100" style={{ width: "100%", height: "17rem"  }}>
-              <div class="sketchfab-embed-wrapper"> 
-                <iframe sandbox="allow-same-origin allow-scripts" width={"100%"} frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="false" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/c6dd9c3ef4a04a698fd5bdd4a47113aa/embed"> 
-                </iframe> 
-                <TimelineItem
-                  title="Surface Scanned Wax Head"
-                  description="Surface scan of wax ecorche sculpture revealing the physical complications of severe and enduring eating disorders."
-                  dateTime="12 March 2023"
-                />
-              </div>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} xl={3}>
-            <Card className="h-100" style={{ width: "100%", height: "17rem"  }}>
-              <div class="sketchfab-embed-wrapper"> 
-                <iframe sandbox="allow-same-origin allow-scripts" width={"100%"} frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="false" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/26f31197479e4d76aa7e5302f386e16f/embed"> 
-                </iframe> 
-                <TimelineItem
-                  title="Ubiquinone Oxidoreductase"
-                  description="Ubiquinone oxidoreductase is an enzyme that catalyzes the chemical reactions of the electron transport chain."
-                  dateTime="12 March 2023"
-                />
-              </div>
-            </Card>
-          </Grid>
+      {modules.map((item, index) => (
+                  <Grid item key={index} xs={12} sm={6} md={5} lg={4} xl={3}>
+                  <Card className="h-100" style={{ width: "100%", height: "19rem"  }}>
+                   
+                    <div class="sketchfab-embed-wrapper embed-responsive embed-responsive-4by3">
+                      
+                      <img src={item.thumbnails.images[2].url} title="A 3D model" alt="A 3D model" className="sketchfab-embed-placeholder" style={{ width: "100%", height: "8rem", objectFit: "cover" }}></img>
+                      <TimelineItem
+                        title={item.name}
+                        // only show the first 100 characters of the description
+                        description={item.description.substring(0, 50)}
+                        dateTime={item.createdAt}
+                      />
+                    </div>
+                    <SuiButton variant="outlined" fontWeight="300" color="info" onClick={() => handleNavigateModule(item.embedUrl)} sx={
+                        {
+                            width: "96%",
+                            fontSize: "12px",
+                            textTransform: "none",
+                            borderRadius: "10px",
+                            position : "absolute",
+                            bottom: "2px",
+                            left: "5px",
+                        }
+                        
+                    }>View</SuiButton>
+                  </Card>
+                  
+                </Grid>
+        ))}
 
         </Grid>
       </SuiBox>
